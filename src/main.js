63b3c690d4966c7705ae4fed1adc546174516523
Jiyeong Son <sgsgk1213@naver.com>
2025-04-11 01:34:05 +0900

    feat: vue와 spring boot 연동

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import router from './router'
 
 import 'bootstrap/dist/css/bootstrap.css'                 
 import 'bootstrap/dist/js/bootstrap.bundle'              
 import 'bootstrap-icons/font/bootstrap-icons.css'       
 
-createApp(App).mount('#app')
+const app = createApp(App).mount('#app')
+
+app.use(router)
+app.mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,27 @@
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080', // Spring Boot 서버 주소
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
