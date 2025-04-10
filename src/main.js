38214beea920a9068e122825c3d8974db20dc7c2
Jiyeong Son <sgsgk1213@naver.com>
2025-04-11 01:30:52 +0900

    chore: 부트스트랩 및 추가 설정

--- src/main.js
+++ src/main.js
@@ -2,4 +2,8 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
+import 'bootstrap/dist/css/bootstrap.css'                 
+import 'bootstrap/dist/js/bootstrap.bundle'              
+import 'bootstrap-icons/font/bootstrap-icons.css'       
+
 createApp(App).mount('#app')
